46449fc3bb56e00b08fccedd8a80d534a90c9544
Muslimchris <muslimchris650@gmail.com>
2025-02-06 09:44:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,29 +5,29 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "GMAX-MD~jdwWVDaK#Efbh6BWtbKqgyF9lSDoRXrj5MuFI40tSwVTj6vLB2Cs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY GMAX🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Grey*",
 // set the auto reply massage on status reply  
-PREFIX: process.env.PREFIX || ",",
+PREFIX: process.env.PREFIX || "_",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "GMAX-MD",
+BOT_NAME: process.env.BOT_NAME || "𝐋𝚵𝚯𝚴𝚰𝐃𝚫𝐒-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "GMAX-MD",
+STICKER_NAME: process.env.STICKER_NAME || "♛𝚩𝐋𝚫𝐂𝐊 𝐆𝚪𝚵𝐘 𝐋𝚵𝚯𝚴𝚰𝐃𝚫𝐒♛",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "🇨🇲",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255753853473",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "237680333605",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Mr Gmax",
+OWNER_NAME: process.env.OWNER_NAME || "𓊈♛𝚩𝐋𝚫𝐂𝐊 𝐆𝚪𝚵𝐘 𝐋𝚵𝚯𝚴𝚰𝐃𝚫𝐒♛𓊉",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ɢᴍᴀx*",
 // add bot owner name    
@@ -39,7 +39,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
@@ -51,11 +51,11 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
-DEV: process.env.DEV || "255753853473",
+DEV: process.env.DEV || "237680333605",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
